8c7e6587a838c1d54ad625adc1f47773c9d149e3
rafal <rafal1behrendt@gmail.com>
2021-08-28 20:36:43 +0200

    configure routing, add login component

--- laser-chess-frontend/src/app/app.module.ts
+++ laser-chess-frontend/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent
+    MainNavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
