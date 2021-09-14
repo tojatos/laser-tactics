4ba7e05e158f0decf25107b97e5346a3af1a3c15
Wi1ku <wilczek20@gmail.com>
2021-09-14 21:20:37 +0200

    added CORS i guess

--- laser-chess-backend/main.py
+++ laser-chess-backend/main.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.middleware.cors import CORSMiddleware
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
@@ -50,6 +51,19 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def verify_password(plain_password, hashed_password):
--- laser-chess-backend/setup_env.ps1
+++ laser-chess-backend/setup_env.ps1
@@ -1,3 +1,4 @@
 python -m venv .venv
 .venv/Scripts/activate
+python -m pip install --upgrade pip
 pip install -r requirements.txt
