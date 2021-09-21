e1b2fbe3147fa3fffb13f396a0bea8f775631105
tojatos <tojatos@gmail.com>
2021-09-22 00:12:45 +0200

    shoot laser work init

--- laser-chess-backend/app/game_engine/game.py
+++ laser-chess-backend/app/game_engine/game.py
@@ -15,3 +15,15 @@ class Game:
     def rotate(self, from_cell: CellCoordinates, to_cell: CellCoordinates):
         self.game_state.board.cells[to_cell] = self.game_state.board.cells[from_cell]
         self.game_state.board.cells[from_cell] = None
+
+    def shoot_laser(self, player: Player):
+        cells_list = self.game_state.board.to_serializable().cells
+        laser_cell = next(x for x in cells_list if x.piece.piece_type == PieceType.LASER and x.piece.piece_type == player)
+        laser_coordinates = laser_cell.coordinates
+        # TODO: calculate laser path
+        # TODO: add time to laser path? path as tuple list (time, cell)
+        # TODO: if hit destroy
+        # TODO: add user and game events to game state
+        # TODO: laser path game event and hit game event
+        # self.game_state.board.cells[to_cell] = self.game_state.board.cells[from_cell]
+        # self.game_state.board.cells[from_cell] = None
