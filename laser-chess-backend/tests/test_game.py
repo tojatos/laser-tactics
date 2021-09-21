80bf307a1e401c84bb92e55eb45220855eb37464
tojatos <tojatos@gmail.com>
2021-09-21 14:29:18 +0200

    move piece test done

--- laser-chess-backend/tests/test_game.py
+++ laser-chess-backend/tests/test_game.py
@@ -19,8 +19,14 @@ def test_start_game():
 def test_move_piece():
     initial_state = get_initial_test_game_state()
     game = Game(initial_state)
-    assert game.game_state.board.cells is False #TODO
-    # game.move()
-    # assert game.game_state.is_started is False
-    # game.start_game()
-    # assert game.game_state.is_started is True
+    game.start_game()
+    from_cell_coordinates = CellCoordinates((0, 1))
+    to_cell_coordinates = CellCoordinates((0, 2))
+    cells = game.game_state.board.cells
+    piece = cells[from_cell_coordinates]
+    assert piece is not None
+    assert cells[to_cell_coordinates] is None
+    game.move(from_cell_coordinates, to_cell_coordinates)
+    cells = game.game_state.board.cells
+    assert cells[from_cell_coordinates] is None
+    assert cells[to_cell_coordinates] is piece
