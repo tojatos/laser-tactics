308baf841db2a16e3ece590f915329776d0afd67
tojatos <tojatos@gmail.com>
2021-09-30 17:16:01 +0200

    refactored and added game api tests

--- laser-chess-backend/tests/test_api_game.py
+++ laser-chess-backend/tests/test_api_game.py
@@ -1,3 +1,5 @@
+from dataclasses import asdict
+
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
@@ -32,11 +34,12 @@ app.dependency_overrides[get_db] = override_get_db
 client = TestClient(app)
 
 tokens = []
+game_id = "some_id"
 
 
 @pytest.fixture(autouse=True)
 def run_around_tests():
-    global tokens
+    global tokens, game_id
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
 
@@ -45,7 +48,7 @@ def run_around_tests():
     post_data(
         "/start_game",
         tokens[0],
-        json=dict(game_id="some_id", player_one_id=create_user_datas[0]['username'], player_two_id=create_user_datas[1]['username']),
+        json=dict(game_id=game_id, player_one_id=create_user_datas[0]['username'], player_two_id=create_user_datas[1]['username']),
     )
     yield
     pass
@@ -78,9 +81,12 @@ def post_create_user(create_user_data):
 
 
 def test_shoot_laser():
-    response = post_data("/shoot_laser", tokens[0], json=dict(game_id="some_id"))
+    shoot_laser_request = ShootLaserRequest(game_id)
+    get_game_state_request = GetGameStateRequest(game_id)
+
+    response = post_data("/shoot_laser", tokens[0], json=asdict(shoot_laser_request))
     assert response.status_code == 200
-    response = post_data("/get_game_state", json=dict(game_id="some_id"))
+    response = post_data("/get_game_state", json=asdict(get_game_state_request))
     assert response.status_code == 200, response.text
     game_state_dict = response.json()
 
@@ -89,3 +95,67 @@ def test_shoot_laser():
     assert game_state.board.cells[(5, 0)] is None
     assert game_state.board.cells[(6, 1)] is None
 
+
+def test_move_block():
+    get_game_state_request = GetGameStateRequest(game_id)
+    # request_dict = dict(game_id="some_id", move_from=dict(x=1, y=1), move_to=dict(x=1, y=2))
+    request = MovePieceRequest(game_id, CellCoordinatesSerializable(1, 1), CellCoordinatesSerializable(1, 2))
+    response = post_data("/move_piece", tokens[0], json=asdict(request))
+    assert response.status_code == 200
+    response = post_data("/get_game_state", json=asdict(get_game_state_request))
+    assert response.status_code == 200, response.text
+    game_state_dict = response.json()
+
+    game_state_serializable: GameStateSerializable = GameStateSerializable(**game_state_dict)
+    game_state = game_state_serializable.to_normal()
+    assert game_state.board.cells[(1, 1)] is None
+    assert game_state.board.cells[(1, 2)] is not None
+
+
+def test_move_block_on_own_piece():
+    get_game_state_request = GetGameStateRequest(game_id)
+    request = MovePieceRequest(game_id, CellCoordinatesSerializable(1, 1), CellCoordinatesSerializable(2, 1))
+    response = post_data("/move_piece", tokens[0], json=asdict(request))
+    assert response.status_code != 200
+    response = post_data("/get_game_state", json=asdict(get_game_state_request))
+    assert response.status_code == 200, response.text
+    game_state_dict = response.json()
+
+    game_state_serializable: GameStateSerializable = GameStateSerializable(**game_state_dict)
+    game_state = game_state_serializable.to_normal()
+    assert game_state.board.cells[(1, 1)] is not None
+    assert game_state.board.cells[(2, 1)] is not None
+
+
+def test_rotate_block():
+    get_game_state_request = GetGameStateRequest(game_id)
+    request = RotatePieceRequest(game_id, CellCoordinatesSerializable(1, 1), 90)
+    response = post_data("/rotate_piece", tokens[0], json=asdict(request))
+    assert response.status_code == 200
+    response = post_data("/get_game_state", json=asdict(get_game_state_request))
+    assert response.status_code == 200, response.text
+    game_state_dict = response.json()
+
+    game_state_serializable: GameStateSerializable = GameStateSerializable(**game_state_dict)
+    game_state = game_state_serializable.to_normal()
+    assert game_state.board.cells[(1, 1)] is Piece(PieceType.BLOCK, Player.PLAYER_ONE, 90)
+
+
+def test_rotate_empty():
+    request = RotatePieceRequest(game_id, CellCoordinatesSerializable(3, 3), 90)
+    response = post_data("/rotate_piece", tokens[0], json=asdict(request))
+    assert response.status_code != 200
+
+
+def test_rotate_hypercube():
+    request = RotatePieceRequest(game_id, CellCoordinatesSerializable(4, 4), 90)
+    response = post_data("/rotate_piece", tokens[0], json=asdict(request))
+    assert response.status_code != 200
+
+
+def test_rotate_block_invalid_angle():
+    invalid_angles = [89, 324, -1, 360, 34, 91, 271]
+    for angle in invalid_angles:
+        request = RotatePieceRequest(game_id, CellCoordinatesSerializable(1, 1), angle)
+        response = post_data("/rotate_piece", tokens[0], json=asdict(request))
+        assert response.status_code != 200
