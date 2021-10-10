a25a85446a2a1e62c4f580b3cef0bdf59ed70fa3
tojatos <tojatos@gmail.com>
2021-10-11 00:58:22 +0200

    added env variables to main; adjusted docker-compose to changes in c4046bb

--- laser-chess-backend/app/main.py
+++ laser-chess-backend/app/main.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime, timedelta
 
 import uvicorn
@@ -18,12 +19,19 @@ from app.game_engine.models import *
 from app.game_engine.requests import *
 
 
-# to get a string like this run:
-# openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-API_PREFIX = "/api/v1"
+def get_env(key, fallback):
+    try:
+        return os.environ[key]
+    except KeyError:
+        return fallback
+
+
+SECRET_KEY = get_env('SECRET_KEY', "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7")
+ALGORITHM = get_env('ALGORITHM', "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES = get_env('ACCESS_TOKEN_EXPIRE_MINUTES', 30)
+API_PREFIX = get_env('API_PREFIX', "/api/v1")
+HOST = get_env('HOST', "localhost")
+PORT = get_env('PORT', 8000)
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -220,6 +228,8 @@ async def shoot_laser(request: ShootLaserRequest,
 
 app.include_router(router)
 
+
 if __name__ == "__main__":
+    # models.Base.metadata.drop_all(bind=engine)
     models.Base.metadata.create_all(bind=engine)
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host=HOST, port=PORT)
