907eeb9e2d85e0a907e69f52298915c7a5e483fe
Wi1ku <wilczek20@gmail.com>
2021-10-18 20:03:11 +0200

    some test work

--- laser-chess-backend/tests/test_lobby_api.py
+++ laser-chess-backend/tests/test_lobby_api.py
@@ -3,7 +3,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.core.database import Base
-from app.game_engine.models import *
 from app.main import app, get_db, API_PREFIX
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
@@ -46,11 +45,24 @@ def get_data(path: str, token: str = None, **kwargs):
 
 
 def test_create_lobby_happy():
-    pass
+    response = post_data("/users/", json={"username": "test", "email": "test@example.com", "password": "admin123"})
+    assert response.status_code == 200
+    response = post_data(
+        "/token",
+        data={"username": "test", "password": "admin123"},
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert "access_token" in data
+    token = data["access_token"]
+
+    response = post_data("/lobby/create", token)
+    assert response.status_code == 200
 
 
 def test_create_lobby_unauthorized():
-    pass
+    response = post_data("/lobby/create", "1234")
+    assert response.status_code == 403
 
 
 def test_join_lobby_happy():
