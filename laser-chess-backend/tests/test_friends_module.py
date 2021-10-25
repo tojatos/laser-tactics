354af08a6617b10571617c1d327062e36cc0dd72
Wi1ku <wilczek20@gmail.com>
2021-10-26 01:46:13 +0200

    changed tests to resolve merge with master

--- laser-chess-backend/tests/test_friends_module.py
+++ laser-chess-backend/tests/test_friends_module.py
@@ -1,7 +1,65 @@
 import pytest
-from tests.utils import Base, engine, post_create_user, post_data, get_data
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.database import Base
+from app.main import app, get_db, API_PREFIX
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+client = TestClient(app)
 
 tokens = []
+
+
+def make_request(method: str, path: str, token: str = None, **kwargs):
+    if token is None:
+        return client.request(method, API_PREFIX + path, **kwargs)
+    else:
+        return client.request(method, API_PREFIX + path, headers={"Authorization": f"Bearer {token}"}, **kwargs)
+
+
+def post_data(path: str, token: str = None, **kwargs):
+    return make_request('POST', path, token, **kwargs)
+
+
+def get_data(path: str, token: str = None, **kwargs):
+    return make_request('GET', path, token, **kwargs)
+
+
+def patch_data(path: str, token: str = None, **kwargs):
+    return make_request('PATCH', path, token, **kwargs)
+
+
+def get_token_data(create_user_data):
+    return dict(username=create_user_data['username'], password=create_user_data['password'])
+
+
+def post_create_user(create_user_data):
+    post_data("/users/", json=create_user_data)
+    response = post_data("/token", data=get_token_data(create_user_data))
+    token = response.json()["access_token"]
+    return token
 
 
 @pytest.fixture(autouse=True)
--- laser-chess-backend/tests/test_lobby_api.py
+++ laser-chess-backend/tests/test_lobby_api.py
@@ -1,5 +1,33 @@
 import pytest
-from tests.utils import Base, engine, post_data, patch_data, post_create_user, get_data
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.database import Base
+from app.main import app, get_db, API_PREFIX
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
+
+client = TestClient(app)
 
 tokens = []
 
@@ -17,16 +45,46 @@ def run_around_tests():
     pass
 
 
+def make_request(method: str, path: str, token: str = None, **kwargs):
+    if token is None:
+        return client.request(method, API_PREFIX + path, **kwargs)
+    else:
+        return client.request(method, API_PREFIX + path, headers={"Authorization": f"Bearer {token}"}, **kwargs)
+
+
+def post_data(path: str, token: str = None, **kwargs):
+    return make_request('POST', path, token, **kwargs)
+
+
+def get_data(path: str, token: str = None, **kwargs):
+    return make_request('GET', path, token, **kwargs)
+
+
+def patch_data(path: str, token: str = None, **kwargs):
+    return make_request('PATCH', path, token, **kwargs)
+
+
+def get_token_data(create_user_data):
+    return dict(username=create_user_data['username'], password=create_user_data['password'])
+
+
+def post_create_user(create_user_data):
+    post_data("/users/", json=create_user_data)
+    response = post_data("/token", data=get_token_data(create_user_data))
+    token = response.json()["access_token"]
+    return token
+
+
 def test_create_lobby_happy():
     response = post_data("/lobby/create", tokens[0])
-    assert response.status_code == 200
+    assert response.status_code == 201
 
 
 def test_get_lobbys():
     response = post_data("/lobby/create", tokens[0])
-    assert response.status_code == 200
+    assert response.status_code == 201
     response = post_data("/lobby/create", tokens[0])
-    assert response.status_code == 200
+    assert response.status_code == 201
     response = get_data("/lobby")
     assert response.status_code == 200
     print(response.json())
@@ -39,14 +97,14 @@ def test_create_lobby_unauthorized():
 
 def test_join_lobby_happy():
     response = post_data("/lobby/create", tokens[0])
-    assert response.status_code == 200
+    assert response.status_code == 201
     response = patch_data(f"/lobby/join?lobby_id={response.json()['id']}", tokens[1])
     assert response.status_code == 200
 
 
 def test_join_lobby_unauthorized():
     response = post_data("/lobby/create", tokens[0])
-    assert response.status_code == 200
+    assert response.status_code == 201
 
     response = patch_data(f"/lobby/join?lobby_id={response.json()['id']}", tokens[1])
     assert response.status_code == 200
@@ -54,7 +112,7 @@ def test_join_lobby_unauthorized():
 
 def test_join_lobby_full():
     response = post_data("/lobby/create", tokens[0])
-    assert response.status_code == 200
+    assert response.status_code == 201
 
     response = patch_data(f"/lobby/join?lobby_id={response.json()['id']}", tokens[1])
     assert response.status_code == 200
@@ -65,7 +123,7 @@ def test_join_lobby_full():
 
 def test_join_lobby_mulitiple():
     response_create = post_data("/lobby/create", tokens[0])
-    assert response_create.status_code == 200
+    assert response_create.status_code == 201
 
     response = patch_data(f"/lobby/join?lobby_id={response_create.json()['id']}", tokens[1])
     assert response.status_code == 200
