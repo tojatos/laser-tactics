73efffe08f9d2298f9d967b4e983520c1da5676e
Wi1ku <wilczek20@gmail.com>
2021-10-26 14:19:42 +0200

    tests are speed now

--- laser-chess-backend/tests/test_friends_module.py
+++ laser-chess-backend/tests/test_friends_module.py
@@ -1,113 +1,65 @@
+
 import pytest
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from app.core.database import Base
+from tests.conftest import engine, TestingSessionLocal
+from tests.utils import *
 from app.main import app, get_db, API_PREFIX
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base.metadata.drop_all(bind=engine)
-Base.metadata.create_all(bind=engine)
-
-
-def override_get_db():
-    try:
-        db = TestingSessionLocal()
-        yield db
-    finally:
-        db.close()
-
-
-app.dependency_overrides[get_db] = override_get_db
-
-client = TestClient(app)
 
 tokens = []
 
 
-def make_request(method: str, path: str, token: str = None, **kwargs):
-    if token is None:
-        return client.request(method, API_PREFIX + path, **kwargs)
-    else:
-        return client.request(method, API_PREFIX + path, headers={"Authorization": f"Bearer {token}"}, **kwargs)
-
-
-def post_data(path: str, token: str = None, **kwargs):
-    return make_request('POST', path, token, **kwargs)
-
-
-def get_data(path: str, token: str = None, **kwargs):
-    return make_request('GET', path, token, **kwargs)
-
-
-def patch_data(path: str, token: str = None, **kwargs):
-    return make_request('PATCH', path, token, **kwargs)
-
-
-def get_token_data(create_user_data):
-    return dict(username=create_user_data['username'], password=create_user_data['password'])
-
-
-def post_create_user(create_user_data):
-    post_data("/users/", json=create_user_data)
-    response = post_data("/token", data=get_token_data(create_user_data))
-    token = response.json()["access_token"]
-    return token
-
-
-@pytest.fixture(autouse=True)
-def run_around_tests():
+@pytest.fixture(scope="session", autouse=True)
+def before_all():
     global tokens
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
+
+    connection = engine.connect()
+    session = TestingSessionLocal(bind=connection)
+
+    def override_get_db():
+        yield session
+
+    app.dependency_overrides[get_db] = override_get_db
+    client = TestClient(app)
+    tu = TestUtils(client, API_PREFIX)
 
     create_user_datas = list(
-        map(lambda x: dict(username=f"test{x}", email=f"test{x}@example.com", password=f"test{x}"), range(0, 5)))
-    tokens = list(map(lambda create_user_data: post_create_user(create_user_data), create_user_datas))
-    yield
-    pass
+        map(lambda x: dict(username=f"test{x}", email=f"test{x}@example.com", password=f"test{x}"), range(0, 3)))
+    tokens = list(map(lambda create_user_data: tu.post_create_user(create_user_data), create_user_datas))
+    session.commit()
 
 
-def test_get_friends_of_user():
-    response = get_data("/users/me/friends", tokens[0])
+def test_get_friends_of_user(tu):
+    response = tu.get_data("/users/me/friends", tokens[0])
     assert response.status_code == 200
 
 
-def test_send_friend_request():
-    response = post_data("/users/me/friends/requests/send?friend_username=test2", tokens[0])
+def test_send_friend_request(tu):
+    response = tu.post_data("/users/me/friends/requests/send?friend_username=test2", tokens[0])
     assert response.status_code == 201
 
 
-def test_get_pending_requests():
-    response = post_data("/users/me/friends/requests/send?friend_username=test2", tokens[0])
+def test_get_pending_requests(tu):
+    response = tu.post_data("/users/me/friends/requests/send?friend_username=test2", tokens[0])
     assert response.status_code == 201
 
-    response = get_data("/users/me/friends", tokens[2])
+    response = tu.get_data("/users/me/friends", tokens[2])
     assert response.status_code == 200
 
 
-def test_accept_friend_request():
-    response = post_data("/users/me/friends/requests/send?friend_username=test2", tokens[0])
+def test_accept_friend_request(tu):
+    response = tu.post_data("/users/me/friends/requests/send?friend_username=test2", tokens[0])
     assert response.status_code == 201
 
-    response = get_data("/users/me/friends/requests", tokens[2])
+    response = tu.get_data("/users/me/friends/requests", tokens[2])
     assert response.status_code == 200
 
     # thats wack
-    response = post_data(f"/users/me/friends/requests/accept?request_id={response.json()[0]['id']}", tokens[2])
+    response = tu.post_data(f"/users/me/friends/requests/accept?request_id={response.json()[0]['id']}", tokens[2])
     assert response.status_code == 200
 
-    response = get_data("/users/me/friends", tokens[0])
+    response = tu.get_data("/users/me/friends", tokens[0])
     user_1_friends = list(response.json())
 
-    response = get_data("/users/me/friends", tokens[2])
+    response =tu.get_data("/users/me/friends", tokens[2])
     user_2_friends = list(response.json())
 
     assert "test0" in user_2_friends
