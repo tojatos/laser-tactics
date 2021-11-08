c7a6900b670ea5017ab8a733ac4ab9a809a14e4b
tojatos <tojatos@gmail.com>
2021-11-08 01:31:06 +0000

    root path and api url updated

--- laser-chess-frontend/src/environments/environment.prod.ts
+++ laser-chess-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: "http://localhost",
-  WEBSOCKET_URL: "ws://localhost/ws",
+  API_URL: "http://got-pttk.icu/laser-tactics-api",
+  WEBSOCKET_URL: "ws://got-pttk.icu/laser-tactics-api/ws",
   API_PREFIX: "/api/v1"
 };
