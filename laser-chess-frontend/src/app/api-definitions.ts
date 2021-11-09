e3a585df80ea321cdd1ab6f8a1d678e7ca523fd8
Małgorzata Orzeł <61987706+Elunari@users.noreply.github.com>
2021-11-09 17:33:51 +0100

    Merge pull request #25 from tojatos/lobby_front
    init lobby and lobby list

--- laser-chess-frontend/src/app/api-definitions.ts
+++ laser-chess-frontend/src/app/api-definitions.ts
@@ -1,9 +1,16 @@
 import { environment } from "src/environments/environment";
 
-export const fullEndpoint = (endpoint: String) => `${environment.API_URL}${environment.API_PREFIX}${endpoint}`
+export const fullEndpoint = (endpoint: String, sub: String = "/") => `${environment.API_URL}${environment.API_PREFIX}${endpoint}${sub}`
 
 export const usersEndpoint = '/users'
 export const tokenEndpoint = '/token'
+export const lobbyEndpoint = '/lobby'
+export const updateLobbyEndpoint = '/lobby/update'
+export const createLobbyEndpoint = '/lobby/create'
+export const joinLobbyEndpoint = '/lobby/join'
+export const userEndpoint = '/users'
+export const gameEnpoint = '/game'
+export const startGameEndpoint = '/start_game'
 export const gameStateEndpoint = '/get_game_state'
 export const movePieceEndpoint = '/move_piece'
 export const rotatePieceEndpoint = '/rotate_piece'
@@ -17,4 +24,13 @@ export const gameStateFullEndpoint = fullEndpoint(gameStateEndpoint)
 export const movePieceFullEndpoint = fullEndpoint(movePieceEndpoint)
 export const rotatePieceFullEndpoint = fullEndpoint(rotatePieceEndpoint)
 export const shootLaserFullEndpoint = fullEndpoint(shootLaserEndpoint)
+export const lobbyFullEndpoint = (id: string = "") => fullEndpoint(lobbyEndpoint, `/${id}`)
+export const updateLobbyFullEndpoint = (id: string = "") => fullEndpoint(updateLobbyEndpoint, `/${id}`)
+export const createLobbyFullEndpoint = (id: string = "") => fullEndpoint(createLobbyEndpoint, `/${id}`)
+export const joinLobbyFullEndpoint = (id: string = "") => fullEndpoint(joinLobbyEndpoint, `/${id}`)
+export const gameFullEndpoint = (id: string  = "") => fullEndpoint(gameEnpoint, `/${id}`)
+export const startGameFullEndpoint = fullEndpoint(startGameEndpoint)
+export const userFullEndpoint = (id: string  = "") => fullEndpoint(userEndpoint, `/${id}`)
+
+
 
---
+++ laser-chess-frontend/src/app/components/lobby/lobby.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Lobby, User } from 'src/app/app.models';
+import { LobbyService } from 'src/app/services/lobby.service';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-lobby',
+  templateUrl: './lobby.component.html',
+  styleUrls: ['./lobby.component.scss']
+})
+export class LobbyComponent implements OnInit, OnDestroy {
+
+  constructor(private lobbyService: LobbyService, private route: ActivatedRoute, private router: Router, private userService: UserService) { }
+
+  lobby: Lobby | undefined
+  refresh = true
+  player_one: string | undefined
+  player_two: string | undefined
+  
+  ngOnInit(): void {
+    this.route.params.subscribe(async params => {
+      this.refreshLobbyState(params.id)
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.refresh = false
+  }
+
+  async changePlayers(){
+    if (this.lobby) {
+      this.lobby.starting_position_reversed = !this.lobby.starting_position_reversed
+      await this.lobbyService.updateLobby(this.lobby)    
+    }
+  }
+
+  changePrivacy() {
+    if (this.lobby) {
+      this.lobby.is_private = true
+      this.lobbyService.updateLobby(this.lobby)
+    }
+  }
+
+  startGame() {
+    if (this.lobby &&  this.player_one && this.player_two) {
+      this.lobbyService.startGame(this.lobby.game_id, this.player_one, this.player_two)
+      this.router.navigate(['/game', this.lobby.game_id])
+    }
+
+  }
+
+  async refreshLobbyState(gameId: string){
+    const lobbyData = await this.lobbyService.getLobbyById(gameId)
+    this.lobby = lobbyData
+    this.player_one = this.lobby.player_one_username
+    this.player_two = this.lobby.player_two_username
+    if(this.lobby?.starting_position_reversed){
+      const p1 = this.player_one
+      this.player_one = this.player_two
+      this.player_two = p1
+    }
+    //console.log(this.lobby)
+    if(this.refresh)
+      window.setTimeout(() => this.refreshLobbyState(gameId), 500)
+  }
+}
