18fac9da6d34ced71c11facb99eed774f6d53521
rafal <rafal1behrendt@gmail.com>
2021-11-22 03:36:29 +0100

    Add piece move test

--- laser-chess-frontend/cypress/integration/spec.ts
+++ laser-chess-frontend/cypress/integration/spec.ts
@@ -8,26 +8,57 @@ before(() => {
       "password": "string"
     }
   }).then(res => {
+    const access_token = res.body.access_token
+    localStorage.setItem('access_token', access_token)
     cy.request({ // create and start game
       method: 'POST',
       url: `${Cypress.env('EXTERNAL_API')}${Cypress.env('API_PREFIX')}${Cypress.env('START_GAME_URL')}`,
       headers: {
-        "Authorization" : `Bearer ${res.body.access_token}`
+        "Authorization" : `Bearer ${access_token}`
       },
       body: {
-        "game_id": "test",
+        "game_id": "test2",
         "player_one_id": "string",
-        "player_two_id": "string"
+        "player_two_id": "string2"
       }
     })
   })
-
-
-
 })
 
 describe('Gameplay tests', () => {
-  it('Loads game', () => {
-    cy.visit("/game/string")
+
+  const getCell = (gc: any, posX: number, posY: number) =>  gc.game.board.cells.find((c : any) => c.coordinates.x == posX && c.coordinates.y == posY)
+
+  it('Test moving', () => {
+    let angular: any
+
+    cy.visit("/game/test2")
+    cy.get('canvas')
+    .then(() => {
+      cy.window().then(win => {
+        angular = (win as any).ng;
+      })
+      .then(() => cy.document())
+      .then((doc) => {
+        const gameComponent = angular.getComponent(doc.querySelector("app-board"))
+        const pressPosition = getCell(gameComponent, 5, 1)
+        const pressPositionNext = getCell(gameComponent, 5, 2)
+        expect(pressPosition.piece).to.be.ok
+        expect(pressPositionNext.piece).to.be.not.ok
+
+        cy.get('canvas').click(pressPosition.canvasCoordinates.x, pressPosition.canvasCoordinates.y)
+        .click(pressPositionNext.canvasCoordinates.x, pressPositionNext.canvasCoordinates.y)
+        .then(() => {
+          cy.wait(1000)
+            .then(() => {
+              expect(pressPosition.piece).to.be.not.ok
+              expect(pressPositionNext.piece).to.be.ok
+            })
+        })
+      })
+    })
+
   })
 })
+
+
--- laser-chess-frontend/cypress/plugins/index.ts
+++ laser-chess-frontend/cypress/plugins/index.ts
@@ -1,3 +1,7 @@
 // Plugins enable you to tap into, modify, or extend the internal behavior of Cypress
 // For more info, visit https://on.cypress.io/plugins-api
-module.exports = (on, config) => {}
+const initCypressMousePositionPlugin = require('cypress-mouse-position/plugin');
+
+module.exports = (on, config) => {
+  initCypressMousePositionPlugin(on);
+}
