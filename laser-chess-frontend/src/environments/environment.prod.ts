65c981b54f8eb20992108c28449732f9dfb267e8
tojatos <tojatos@gmail.com>
2025-01-18 16:27:41 +0100

    Fix prod chat WS url

--- laser-chess-frontend/src/environments/environment.prod.ts
+++ laser-chess-frontend/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   API_URL: "https://krzysztofruczkowski.pl/laser-tactics-api",
   WEBSOCKET_URL: "wss://krzysztofruczkowski.pl/laser-tactics-api/ws",
-  CHAT_WEBSOCKET_URL: "ws://krzysztofruczkowski.pl/laser-tactics-api/chat",
+  CHAT_WEBSOCKET_URL: "wss://krzysztofruczkowski.pl/laser-tactics-api/chat",
   LOBBY_WEBSOCKET_URL: "wss://krzysztofruczkowski.pl/laser-tactics-api/lobby_ws",
   API_PREFIX: "/api/v1",
 };
